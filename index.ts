38908958a07ad00a044f40abeca81618cb46f02c
Daniel Chistyakov <4i.danila@gmail.com>
2022-03-30 00:04:55 +0300

    CORS fixed

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import checkVersion from './routes/checkVersion';
 import checkImage from './routes/checkImage';
-
+import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(express.json());
