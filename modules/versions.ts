1c4825bb0fa7f92bf457c03fce3e8fd7283e0de7
Daniel Chistyakov <4i.danila@gmail.com>
2022-03-29 21:06:03 +0300

    Text added

--- modules/versions.ts
+++ modules/versions.ts
@@ -1,3 +1,3 @@
 export const versions = {
-  '0.0.1': { isActual: false, isAllowed: false, title: '', description: '' },
+  '0.0.1': { isActual: false, isAllowed: true, title: '', description: '' },
 };
