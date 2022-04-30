1b25e66b1a575abf837dead2e0092dac9d504738
Stanislav Zhernosek <revinsys@yandex.ru>
2022-04-30 20:11:32 +0300

    added users post, change mutation to query

--- src/core/user/user.module.ts
+++ src/core/user/user.module.ts
@@ -5,9 +5,15 @@ import { UserEntity } from './user.entity';
 import { UserResolver } from './user.resolver';
 import { MediaModule } from '../media/media.module';
 import { SearchModule } from '../../api/search/search.module';
+import { PostModule } from '../post/post.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), MediaModule, SearchModule],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    MediaModule,
+    SearchModule,
+    PostModule,
+  ],
   providers: [UserService, UserResolver],
   exports: [UserService],
 })
